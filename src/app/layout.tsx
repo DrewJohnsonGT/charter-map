a147d07d099a2eb4761cfdb8ad0fed4ea1cb6fc9
Drew Johnson <drewjohnsongt@gmail.com>
2024-02-05 19:44:23 -0500

    Got base styles and map layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Relentless Charters Map",
-  description: "Map of experiences with Relentless Charters",
+  title: 'Relentless Charters Map',
+  description: 'Map of experiences with Relentless Charters',
 };
 
 export default function RootLayout({
@@ -16,6 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://api.tiles.mapbox.com/mapbox-gl-js/v3.1.2/mapbox-gl.css"
+          rel="stylesheet"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,33 +1,11 @@
 import React, { useState, useCallback } from 'react';
-import ReactMapGL, { ViewState, Source, Layer } from 'react-map-gl';
-import type { CircleLayer } from 'react-map-gl';
+import ReactMapGL, { ViewState, Marker } from 'react-map-gl';
 import { booleanPointInPolygon, circle } from '@turf/turf';
-import type { FeatureCollection } from 'geojson';
 import MapBoxGL from 'mapbox-gl';
 
 const LATITUDE = 21.7703;
 const LONGITUDE = -71.9041;
 const GEOFENCE = circle([LONGITUDE, LATITUDE], 50, { units: 'miles' });
-
-const geojson: FeatureCollection = {
-  type: 'FeatureCollection',
-  features: [
-    {
-      type: 'Feature',
-      geometry: { type: 'Point', coordinates: [LATITUDE, LONGITUDE] },
-      properties: { name: 'TESTING' },
-    },
-  ],
-};
-
-const layerStyle: CircleLayer = {
-  id: 'point',
-  type: 'circle',
-  paint: {
-    'circle-radius': 10,
-    'circle-color': '#007cbf',
-  },
-};
 
 export const Map = () => {
   const [viewState, setViewState] = useState<Partial<ViewState>>({
@@ -43,20 +21,19 @@ export const Map = () => {
   }, []);
   return (
     <>
-      {JSON.stringify(viewState)}
       <ReactMapGL
         {...viewState}
         mapLib={MapBoxGL}
         onMove={onMove}
-        style={{ maxHeight: '100%', maxWidth: '90%' }}
+        style={{ maxHeight: '100%', maxWidth: '100%' }}
         mapStyle="mapbox://styles/mapbox/streets-v9"
         mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_PUBLIC_TOKEN}
         attributionControl={false}
         minZoom={8}
         reuseMaps>
-        <Source id="my-data" type="geojson" data={geojson}>
-          <Layer {...layerStyle} />
-        </Source>
+        <Marker longitude={LONGITUDE} latitude={LATITUDE} anchor="bottom">
+          <div>📍</div>
+        </Marker>
       </ReactMapGL>
     </>
   );
