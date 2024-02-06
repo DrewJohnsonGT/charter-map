3cbe6078e28dc9136268d020006af0730102f76e
Drew Johnson <drewjohnsongt@gmail.com>
2024-02-05 19:32:55 -0500

    Switching to react-map-gl

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,25 +1,63 @@
-import React, { ComponentType } from 'react';
-import dynamic from 'next/dynamic';
+import React, { useState, useCallback } from 'react';
+import ReactMapGL, { ViewState, Source, Layer } from 'react-map-gl';
+import type { CircleLayer } from 'react-map-gl';
+import { booleanPointInPolygon, circle } from '@turf/turf';
+import type { FeatureCollection } from 'geojson';
+import MapBoxGL from 'mapbox-gl';
 
-export interface MapProps {
-  position: [number, number];
-  zoom: number;
-  width?: number;
-  height?: number;
-}
+const LATITUDE = 21.7703;
+const LONGITUDE = -71.9041;
+const GEOFENCE = circle([LONGITUDE, LATITUDE], 50, { units: 'miles' });
 
-const DynamicMap: ComponentType<MapProps> = dynamic(() => import('./Map'), {
-  ssr: false,
-});
+const geojson: FeatureCollection = {
+  type: 'FeatureCollection',
+  features: [
+    {
+      type: 'Feature',
+      geometry: { type: 'Point', coordinates: [LATITUDE, LONGITUDE] },
+      properties: { name: 'TESTING' },
+    },
+  ],
+};
 
-const DEFAULT_WIDTH = 600;
-const DEFAULT_HEIGHT = 600;
+const layerStyle: CircleLayer = {
+  id: 'point',
+  type: 'circle',
+  paint: {
+    'circle-radius': 10,
+    'circle-color': '#007cbf',
+  },
+};
 
-export const Map = (props: MapProps) => {
-  const { width = DEFAULT_WIDTH, height = DEFAULT_HEIGHT } = props;
+export const Map = () => {
+  const [viewState, setViewState] = useState<Partial<ViewState>>({
+    longitude: LONGITUDE,
+    latitude: LATITUDE,
+    zoom: 10,
+  });
+  const onMove = useCallback(({ viewState }: { viewState: ViewState }) => {
+    const newCenter = [viewState.longitude, viewState.latitude];
+    if (booleanPointInPolygon(newCenter, GEOFENCE)) {
+      setViewState(viewState);
+    }
+  }, []);
   return (
-    <div style={{ aspectRatio: width / height }}>
-      <DynamicMap {...props} />
-    </div>
+    <>
+      {JSON.stringify(viewState)}
+      <ReactMapGL
+        {...viewState}
+        mapLib={MapBoxGL}
+        onMove={onMove}
+        style={{ maxHeight: '100%', maxWidth: '90%' }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_PUBLIC_TOKEN}
+        attributionControl={false}
+        minZoom={8}
+        reuseMaps>
+        <Source id="my-data" type="geojson" data={geojson}>
+          <Layer {...layerStyle} />
+        </Source>
+      </ReactMapGL>
+    </>
   );
 };
---
+++ src/environment.d.ts
@@ -0,0 +1,5 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_MAPBOX_PUBLIC_TOKEN: string;
+  }
+}
